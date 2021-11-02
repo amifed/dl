7fbc8995c9e83582ee3066b1cc889afcee629f0d
iamfed <iamjyding@gmail.com>
2021-11-02 21:41:49 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # deep learning for image processing
 
+运行程序
+
+```shell
+python3 app.py
+```
+
+app.py 会调用系统命令，在后台执行 main.py 文件，main.py 中使用 `print()` 函数输出的内容都会写到 `.result\{执行脚本时间}` 文件夹下的 log.out 文件中，同时会保存训练结束后的模型
+
 经卷积后的矩阵尺寸大小计算公式为
 
 $$
--- app.py
+++ app.py
@@ -3,12 +3,15 @@ import sys
 import time
 import getopt
 
+# 记录执行时的当前时间
 now = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime(time.time()))
 
 root = os.path.abspath('.')
 result_dir = os.path.join(root, 'result', now)
 log_path = os.path.join(result_dir, 'log.out')
+# 新建输出文件夹
 os.mkdir(result_dir)
 
+# 调用系统命令，后台训练模型，训练 log 输出到 log_path 中
 command = f'nohup python3 main.py -d {result_dir} > {log_path} &'
 os.system(command)

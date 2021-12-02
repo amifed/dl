3909abdf9fa79349dd928c63ae9c6d7b4a5acdeb
iamfed <iamjyding@gmail.com>
2021-12-02 13:27:37 +0800

    fix no parallel folder bugsx

--- utils/data/ParallelImageFolder.py
+++ utils/data/ParallelImageFolder.py
@@ -1,3 +1,4 @@
+import torch
 import torchvision
 import os
 
@@ -20,14 +21,17 @@ class ParallelImageFolder(torchvision.datasets.ImageFolder):
         sample = self.loader(path)
 
         _, parallel_name = os.path.split(path)
-        parallel_path = os.path.join(os.path.join(
-            self.parallel_root, self.classes[target], parallel_name))
-        parallel_sample = self.loader(parallel_path)
+        parallel_path, parallel_sample = None, None
+        if self.parallel_root is not None:
+            parallel_path = os.path.join(os.path.join(
+                self.parallel_root, self.classes[target], parallel_name))
+            parallel_sample = self.loader(parallel_path)
 
         if self.transform is not None:
             sample = self.transform(sample)
-            parallel_sample = self.transform(parallel_sample)
+            if parallel_sample is not None:
+                parallel_sample = self.transform(parallel_sample)
         if self.target_transform is not None:
             target = self.target_transform(target)
 
-        return sample, target, parallel_sample
+        return sample, target, parallel_sample or []

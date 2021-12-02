a0986c7c0e7ee3db6b0f4c7afa31536eb1a90f3b
iamfed <iamjyding@gmail.com>
2021-12-02 13:24:36 +0800

    alexnet args reset

--- models/alexnet.py
+++ models/alexnet.py
@@ -17,10 +17,10 @@ class AlexNet(nn.Module):
     def __init__(self, num_classes: int = 1000, dropout: float = 0.5) -> None:
         super().__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=2),
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(64, 192, kernel_size=3, padding=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(192, 384, kernel_size=3, padding=1),
@@ -31,19 +31,19 @@ class AlexNet(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
         )
-        self.avgpool = nn.AdaptiveAvgPool2d((3, 3))
-        self.classifier1 = nn.Sequential(
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = nn.Sequential(
             nn.Dropout(p=dropout),
-            nn.Linear(256 * 3 * 3, 4096),
+            nn.Linear(256 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
             nn.Dropout(p=dropout),
             nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
             nn.Linear(4096, num_classes),
         )
-        self.classifier2 = nn.Sequential(
+        self.__classifier__ = nn.Sequential(
             nn.Dropout(p=dropout),
-            nn.Linear(2 * 256 * 3 * 3, 4096),
+            nn.Linear(2 * 256 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
             nn.Dropout(p=dropout),
             nn.Linear(4096, 4096),
@@ -52,20 +52,19 @@ class AlexNet(nn.Module):
         )
 
     def forward(self, x: torch.Tensor, y: torch.Tensor = None) -> torch.Tensor:
-        # if not self.training:
-        #     x = self.features(x)
-        #     x = self.avgpool(x)
-        #     x = torch.flatten(x, 1)
-        #     x = self.classifier1(x)
-        #     return x
-        x, y = self.features(x), self.features(y)
-        x, y = self.avgpool(x), self.avgpool(y)
+        if y is not None:
+            x, y = self.features(x), self.features(y)
+            x, y = self.avgpool(x), self.avgpool(y)
+            z = torch.cat((x, y), 1)
+            z = torch.flatten(z, 1)
+            z = self.__classifier__(z)
+            return z
 
-        z = torch.cat((x, y), 1)
-
-        z = torch.flatten(z, 1)
-        z = self.classifier2(z)
-        return z
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
 
 
 def alexnet(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> AlexNet:

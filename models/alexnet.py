93d2577a40a45a6818d8a6b935df2b4860b0d11a
iamfed <iamjyding@gmail.com>
2021-12-12 19:07:30 +0800

    reset alexnet

--- models/alexnet.py
+++ models/alexnet.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from typing import Any
+
 try:
     from torch.hub import load_state_dict_from_url
 except ImportError:
@@ -14,8 +15,9 @@ model_urls = {
 
 
 class AlexNet(nn.Module):
-    def __init__(self, num_classes: int = 1000, dropout: float = 0.5) -> None:
-        super().__init__()
+
+    def __init__(self, num_classes: int = 1000) -> None:
+        super(AlexNet, self).__init__()
         self.features = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
             nn.ReLU(inplace=True),
@@ -33,33 +35,16 @@ class AlexNet(nn.Module):
         )
         self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
         self.classifier = nn.Sequential(
-            nn.Dropout(p=dropout),
+            nn.Dropout(),
             nn.Linear(256 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
-            nn.Dropout(p=dropout),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes),
-        )
-        self.__classifier__ = nn.Sequential(
-            nn.Dropout(p=dropout),
-            nn.Linear(2 * 256 * 6 * 6, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(p=dropout),
+            nn.Dropout(),
             nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
             nn.Linear(4096, num_classes),
         )
 
-    def forward(self, x: torch.Tensor, y: torch.Tensor = None) -> torch.Tensor:
-        if y is not None:
-            x, y = self.features(x), self.features(y)
-            x, y = self.avgpool(x), self.avgpool(y)
-            z = torch.cat((x, y), 1)
-            z = torch.flatten(z, 1)
-            z = self.__classifier__(z)
-            return z
-
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.features(x)
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
@@ -80,5 +65,5 @@ def alexnet(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> A
     if pretrained:
         state_dict = load_state_dict_from_url(model_urls['alexnet'],
                                               progress=progress)
-        model.load_state_dict(state_dict, strict=False)
+        model.load_state_dict(state_dict)
     return model
